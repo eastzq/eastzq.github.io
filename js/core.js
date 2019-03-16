2b36b10a27c637c51c3bb7985150f488171f7511
eastzq <eastzq@gmail.com>
2019-03-16 22:01:54 +0800

    新增获取文件树方法

--- js/core.js
+++ js/core.js
@@ -1,98 +1,117 @@
 var issuesList;
 var issuesHTML;
-
-$(document).ready(function() {
-    var webURL = window.location.href;
-    var splitFlag = "http://";
-    if (webURL.substring(0, 5) == "https") {
-        splitFlag = "https://";
-    }
-    var user = webURL.split(splitFlag)[1].split(".")[0];
-    //user = 'yanghanqing';
-    blogListURL = 'https://api.github.com/repos/' + user + '/' + user + '.github.io/contents/blog';
-    issuesList = 'https://api.github.com/repos/' + user + '/' + user + '.github.io/issues';
-    issuesHTML = 'https://github.com/' + user + '/' + user + '.github.io/issues'
-    readmeURL = 'https://raw.githubusercontent.com/' + user + '/' + user + '.github.io/master/About Me.md';
-
-
-    $("#header").text(user + "'s Blog");
-    $("#commentsList").removeAttr('data_comments_url');
-    $("#tips").html("我们不会获取您的用户名和密码,评论直接通过 HTTPS 与 Github API交互,<br>如果您开启了两步验证,请在博客的<a  target=\"_blank\" href=\"" + issuesHTML + "\">Github issues</a>下添加 Comment");
-
-    var titleString = getTitleString();
-
-    //set Blog list    
-    $.getJSON(blogListURL, function(json) {
-        for (var i = 0; i < json.length; i++) {
-            var name = json[i].name; // Blog title
-            var blogURL = json[i].download_url; //Blog Raw Url
-            // add blog list elements
-            var new_li = $("<li></li>");
-            var new_a = $("<a></a>")
-
-            var type = "markdown";
-            // delete '.md'
-            if (name.substr(-3, 3) == ".md") {
-                name = name.substr(0, name.length - 3);
-            } else if (name.substr(-5, 5) == ".html") {
-                name = name.substr(0, name.length - 5);
-                type = "html";
-            }
-            // console.log(name);
-            // console.log(titleString);
-            if (name == titleString) {
-                $("#title").text(name);
-                readmeURL = blogURL;
-            }
-
-            new_a.text(name);
-            //update content
-            new_a.attr("data_blogURL", blogURL);
-            new_a.attr("data_name", name);
-            //new_a.attr("href", "?title=" + name);
-            new_a.attr("href", "#");
-            new_a.attr("data_type", type);
-            new_a.attr("onclick", "setBlogTxt(this)");
-            new_li.append(new_a);
-            $('#nav').append(new_li);
-            $('#nav2').append(new_li.clone());
-
-
-
+var Api = +function(){
+    var M = function(){};
+    M.init = function(){
+        var webURL = window.location.href;
+        var splitFlag = "http://";
+        if (webURL.substring(0, 5) == "https") {
+            splitFlag = "https://";
         }
+        var gh = {};
+        gh.user = webURL.split(splitFlag)[1].split(".")[0];
+        gh.blogListURL = 'https://api.github.com/repos/' + user + '/' + user + '.github.io/contents/blog';
+        gh.issuesList = 'https://api.github.com/repos/' + user + '/' + user + '.github.io/issues';
+        gh.issuesHTML = 'https://github.com/' + user + '/' + user + '.github.io/issues'
+        gh.readmeURL = 'https://raw.githubusercontent.com/' + user + '/' + user + '.github.io/master/About Me.md';
+        var api = new Api();
+        api.gh = gh;
+        api.blogTree= Api.genBlogTree(gh.blogListURL);
+        return api;
+    }    
 
-
-        //set readme 
-        $.get(readmeURL, function(result) {
-            $("#title").show();
-            $("#article").html("");
-
-            testEditormdView = editormd.markdownToHTML("article", {
-                markdown: result, //+ "\r\n" + $("#append-test").text(),
-                // htmlDecode: true, // 开启 HTML 标签解析，为了安全性，默认不开启
-                htmlDecode: "style,script,iframe", // you can filter tags decode
-                //toc             : false,
-                tocm: true, // Using [TOCM]
-                //tocContainer    : "#custom-toc-container", // 自定义 ToC 容器层
-                //gfm             : false,
-                //tocDropdown     : true,
-                // markdownSourceCode : true, // 是否保留 Markdown 源码，即是否删除保存源码的 Textarea 标签
-                emoji: true,
-                taskList: true,
-                tex: true, // 默认不解析
-                flowChart: true, // 默认不解析
-                sequenceDiagram: true, // 默认不解析
-            });
-
-
-
+    //递归生成博客树 效率低，但是目前文件较少，以后可以改成懒加载。
+    M.genBlogTree = function(contentUrl) {
+        var blogTree = [];
+        $.ajax({
+            dataType: "json",
+            url: contentUrl,
+            async: false, 
+            success: function(json) {
+                for (var i = 0; i < json.length; i++) {
+                    var node =  {
+                        text: "",
+                        nodes: []
+                    }                
+                    var obj = json[i];
+                    console.log(obj);
+                    var fileName = obj.name; 
+                    var fileType = obj.type;
+                    if(fileType==='file' && Api.isMarkdown(fileName)){
+                        node.origin = obj;
+                        node.text=obj.name;
+                        node.type= fileType;
+                        node.url=obj.download_url;
+                        blogTree.push(node);
+                    }else if(fileType==='dir'){
+                        node.origin = obj;
+                        node.text=obj.name;
+                        node.type=fileType;
+                        node.nodes = genBlogTree();
+                        blogTree.push(node);
+                    }
+                }
+            }
         });
-    });
+        return blogTree;
+    }
+    M.isMarkdown =function(fileName){
+        var index= fileName.lastIndexOf(".");
+        //获取后缀
+        var ext = fileName.substr(index+1).toLowerCase();
+        //输出结果
+        return (ext==="md" || ext ==="markdown");
+    }   
+    return M;
+}();
+$(document).ready(function() {
+    var main = Api.init();
+    console.log(main);
+});
+
+// 
+//     var webURL = window.location.href;
+//     var splitFlag = "http://";
+//     if (webURL.substring(0, 5) == "https") {
+//         splitFlag = "https://";
+//     }
+//     var user = webURL.split(splitFlag)[1].split(".")[0];
+//     //user = 'yanghanqing';
+//     blogListURL = 'https://api.github.com/repos/' + user + '/' + user + '.github.io/contents/blog';
+//     issuesList = 'https://api.github.com/repos/' + user + '/' + user + '.github.io/issues';
+//     issuesHTML = 'https://github.com/' + user + '/' + user + '.github.io/issues'
+//     readmeURL = 'https://raw.githubusercontent.com/' + user + '/' + user + '.github.io/master/About Me.md';
 
 
+//     $("#header").text(user + "'s Blog");
+//     $("#commentsList").removeAttr('data_comments_url');
+//     $("#tips").html("我们不会获取您的用户名和密码,评论直接通过 HTTPS 与 Github API交互,<br>如果您开启了两步验证,请在博客的<a  target=\"_blank\" href=\"" + issuesHTML + "\">Github issues</a>下添加 Comment");
 
+//     var titleString = getTitleString();
+//         //set readme 
+//     $.get(readmeURL, function(result) {
+//         $("#title").show();
+//         $("#article").html("");
 
-})
+//         testEditormdView = editormd.markdownToHTML("article", {
+//             markdown: result, //+ "\r\n" + $("#append-test").text(),
+//             // htmlDecode: true, // 开启 HTML 标签解析，为了安全性，默认不开启
+//             htmlDecode: "style,script,iframe", // you can filter tags decode
+//             //toc             : false,
+//             tocm: true, // Using [TOCM]
+//             //tocContainer    : "#custom-toc-container", // 自定义 ToC 容器层
+//             //gfm             : false,
+//             //tocDropdown     : true,
+//             // markdownSourceCode : true, // 是否保留 Markdown 源码，即是否删除保存源码的 Textarea 标签
+//             emoji: true,
+//             taskList: true,
+//             tex: true, // 默认不解析
+//             flowChart: true, // 默认不解析
+//             sequenceDiagram: true, // 默认不解析
+//         });
+//     });
+// });
+
 
 
 
