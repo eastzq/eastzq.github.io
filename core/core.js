487dc92bba61349e8f7977582335a0dd9733ec90
eastzq <eastzq@gmail.com>
2019-09-17 10:00:13 +0800

    u

--- core/core.js
+++ core/core.js
@@ -13,16 +13,9 @@ var gh = {
 var Api = (function() {
     var M = function() {};
     M.init = function() {
-        $("#header").text(gh.username + "'s blog");
-        $("#commentsList").removeAttr("data_comments_url");
-        $("#tips").html(
-            '我们不会获取您的用户名和密码,评论直接通过 HTTPS 与 Github API交互,<br>如果您开启了两步验证,请在博客的<a  target="_blank" href="' +
-            issuesHTML +
-            '">Github issues</a>下添加 Comment'
-        );
         Api.genBlogTree2(gh.treeUrl);
         var tid = Api.getUrlParams("tid");
-        Api.renderAboutMe(tid);
+        Api.renderArticle(tid);
         Api.bindEvent();
         $("#article").css("min-height", window.innerHeight - 150 + "px");
         if (location.hash) { M.anchorHandle(location.hash) }
@@ -247,7 +240,7 @@ var Api = (function() {
             renderBlogCommnet();
         }
         var renderBlogCommnet = function(){
-            $("#comment").remove();
+            $("#comments").remove();
             $(".markdwon-content").append('<div class="comments-container" id="comments"></div>');
             var hash = md5(tid);
             var gitalk = new Gitalk({
@@ -287,10 +280,6 @@ var Api = (function() {
                         return r;
                     });
                     renderMd(md);
-                    if (tid) {
-                        var stateObject = {};
-                        history.pushState(stateObject, '', '?tid=' + encodeURIComponent(tid));
-                    }
                     gh.cache[blogUrl] = md;
                 }
             });
@@ -299,12 +288,13 @@ var Api = (function() {
         //get comments_url
         // setCommentURL(issuesList, blogName);
     };
-    M.renderAboutMe = function(tid) {
+    M.renderArticle = function(tid) {
         var node = {
             blogUrl: gh.readmeURL,
             blogPath: "/blog/aboutme/",
             type: "file",
-            fileName: "个人介绍"
+            fileName: "个人介绍",
+            tid:"blog/ABOUT/About Me.md"
         };
         if (tid) {
             var arr = tid.split("/");
@@ -315,6 +305,7 @@ var Api = (function() {
             node.blogPath =
                 "/" + tid.substring(0, tid.lastIndexOf("/") + 1);
             node.blogUrl = gh.baseBlogUrl + tid;
+            node.tid=tid;
             M.renderBlogTxt(node, true);
         } else {
             M.renderBlogTxt(node);
@@ -465,141 +456,3 @@ $(document).ready(function() {
     var main = Api.init();
 });
 
-function setCommentURL(issuesList, blogName) {
-    $("#comments").show();
-    console.log("获取并设置评论区");
-
-    $.ajax({
-        type: "GET",
-        url: issuesList,
-        dataType: "json",
-        async: false,
-        success: function(json) {
-            for (var i = 0; i < json.length; i++) {
-                var title = json[i].title; // Blog title
-                var comments_url = json[i].comments_url;
-                if (title == blogName) {
-                    console.log("该文章存在评论");
-                    $("#commentsList").attr("data_comments_url", comments_url);
-                    setComment(comments_url);
-                    break;
-                }
-                $("#commentsList")
-                    .children()
-                    .remove();
-                $("#commentsList").removeAttr("data_comments_url");
-            }
-        }
-    });
-}
-
-function setComment(commentURL) {
-    $("#commentsList")
-        .children()
-        .remove();
-
-    $.getJSON(commentURL, function(json) {
-        for (var i = 0; i < json.length; i++) {
-            var avatar_url = json[i].user.avatar_url; // avatar_url
-            var user = json[i].user.login;
-            //var updated_at = json[i].updated_at;
-            var updated_at = new Date(json[i].updated_at).toLocaleString();
-            var body = json[i].body;
-
-            // add blog list elements
-            var commentHtml =
-                '<li class="comment">' +
-                '<a class="pull-left" href="#"><img class="avatar" src="' +
-                avatar_url +
-                '" alt="avatar"></a><div class="comment-body"><div class="comment-heading"><h4 class="user">' +
-                user +
-                '</h4><h5 class="time">' +
-                updated_at +
-                "</h5></div><p>" +
-                body +
-                "</p></div></li>";
-
-            var new_obj = $(commentHtml);
-            $("#commentsList").append(new_obj);
-        }
-    });
-}
-
-function login() {
-    $("#myModal").modal();
-}
-
-function subComment() {
-    var USERNAME = $("#txt_username").val();
-    var PASSWORD = document.getElementById("txt_password").value; //
-    var title = null;
-    title = $("#title").text();
-    // 未开启评论
-    if (typeof $("#commentsList").attr("data_comments_url") == "undefined") {
-        if (title == undefined || title == null || title == "") {
-            return;
-        }
-
-        var createIssueJson = '{"title": "' + title + '"}';
-        console.log(createIssueJson);
-        $.ajax({
-            type: "POST",
-            url: issuesList,
-            dataType: "json",
-            async: false,
-            headers: {
-                Authorization: "Basic " + btoa(USERNAME + ":" + PASSWORD)
-            },
-            data: createIssueJson,
-            success: function() {
-                console.log("开启评论成功:" + title);
-                //重新遍历issue list
-                setCommentURL(issuesList, title);
-                console.log("重新遍历 issuesList 完成");
-            }
-        });
-    }
-    console.log("准备提交评论");
-    // 已开启评论
-    if (typeof $("#commentsList").attr("data_comments_url") != "undefined") {
-        var issueURL = $("#commentsList").attr("data_comments_url");
-        var comment = $("#comment_txt").val();
-        var commentJson = '{"body": "' + comment + '"}';
-        console.log(comment);
-        if (comment == "") {
-            alert("评论不能为空");
-            return;
-        }
-
-        $.ajax({
-            type: "POST",
-            url: issueURL,
-            dataType: "json",
-            async: false,
-            headers: {
-                Authorization: "Basic " + btoa(USERNAME + ":" + PASSWORD)
-            },
-            data: commentJson,
-            success: function() {
-                console.log("评论成功");
-
-                // 更新评论区
-                if (title != null) {
-                    setCommentURL(issuesList, title);
-                }
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                alert("账号密码错误,或者开启了两步验证");
-            }
-        });
-    } else {
-        console.log("未开启评论");
-    }
-}
-
-function getTitleString() {
-    var reg = new RegExp("(^|&)" + "title" + "=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) return decodeURI(r[2]);
-    return null;
-}
