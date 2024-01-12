52f5816b9c77e61377776c9f50e92446ef73263e
meowrain <meowrain@126.com>
2024-01-12 12:50:11 +0800

    posts added

---
+++ lib/pdfh5/example/test/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app"><div id="demo"></div></div>
+</template>
+<script>
+import Pdfh5 from 'pdfh5';
+export default {
+  name: 'App',
+  data() {
+    return {
+      pdfh5: null
+    };
+  },
+  mounted() {
+     new Pdfh5('#demo', {
+         pdfurl: "./static/git.pdf"
+       });
+    
+  
+  }
+};
+</script>
+
+<style>
+@import 'pdfh5/css/pdfh5.css';
+* {
+  padding: 0;
+  margin: 0;
+}
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
